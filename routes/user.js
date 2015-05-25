7dea2ed117f86010a36828afce487778a782ea0d
wangrui <wrwangwr@cn.ibm.com>
2015-05-25 13:10:01 +0800

    login function

--- routes/user.js
+++ routes/user.js
@@ -52,4 +52,30 @@ router.get("/list", function(req, res) {
     res.render("list", {users: global.users});
 });
 
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+//登录功能
+router.post("/login", function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // 临时验证 (注:forEach不能break跳出循环)
+    var pass = false;
+    global.users.forEach(function(user) {
+        if(username === user.username && password === user.password) {
+            pass = true;
+        }
+    });
+
+    if(pass) {
+        res.send("login success!");
+        // session
+    } else {
+//        res.send("login false!");
+        res.render("login", {error: true});
+    }
+});
+
 module.exports = router;
